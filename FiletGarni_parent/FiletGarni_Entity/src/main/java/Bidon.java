8413ddd57c405e34d2638de0657e767480083091
Marion22360 <30406064+Marion22360@users.noreply.github.com>
2017-09-14 12:20:21 +0200

    Classe Bidon

---
+++ FiletGarni_parent/FiletGarni_Entity/src/main/java/Bidon.java
@@ -0,0 +1,36 @@
+
+public class Bidon {
+
+    private String pouet;
+    private in age;
+    public Bidon() {
+        // TODO Auto-generated constructor stub
+    }
+    /**
+     * @return the pouet
+     */
+    public String getPouet() {
+        return pouet;
+    }
+    /**
+     * @param paramPouet the pouet to set
+     */
+    public void setPouet(String paramPouet) {
+        pouet = paramPouet;
+    }
+    /**
+     * @return the age
+     */
+    public in getAge() {
+        return age;
+    }
+    /**
+     * @param paramAge the age to set
+     */
+    public void setAge(in paramAge) {
+        age = paramAge;
+    }
+    
+    
+
+}
