e6853618aeee661295ca27f2001c046afbb181ed
Marion22360 <30406064+Marion22360@users.noreply.github.com>
2017-09-19 12:09:39 +0200

    Modification entity jour horaire point relais

--- FiletGarni_parent/FiletGarni_Entity/src/main/java/fr/afcepf/ai101/groupe1/filetGarni/entity/HorairesOuverture.java
+++ FiletGarni_parent/FiletGarni_Entity/src/main/java/fr/afcepf/ai101/groupe1/filetGarni/entity/HorairesOuverture.java
@@ -1,14 +1,15 @@
 package fr.afcepf.ai101.groupe1.filetGarni.entity;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -29,10 +30,22 @@ public class HorairesOuverture implements Serializable {
     private String heureDeFin;
 
     
-    @ManyToMany(mappedBy="horaires")
-    private List<Jour> jours;
+    @ManyToOne
+    @JoinColumn(name="id_jour", nullable=true, foreignKey=
+    @ForeignKey(name="id_jour"))
+    private Jour jour;
     
-    public Integer getId() {
+    @ManyToOne
+    @JoinColumn(name="id_pointrelais", nullable=true, foreignKey=
+    @ForeignKey(name="id_utilisateur"))
+    private PointRelais pointRelais;
+    
+   
+
+	public HorairesOuverture() {
+    }
+
+	public Integer getId() {
 		return id;
 	}
 
@@ -56,23 +69,31 @@ public class HorairesOuverture implements Serializable {
 		this.heureDeFin = heureDeFin;
 	}
 
-	public List<Jour> getJours() {
-		return jours;
+	public Jour getJour() {
+		return jour;
 	}
 
-	public void setJours(List<Jour> jours) {
-		this.jours = jours;
+	public void setJour(Jour jour) {
+		this.jour = jour;
 	}
 
-	public HorairesOuverture() {
-    }
+	public PointRelais getPointRelais() {
+		return pointRelais;
+	}
 
-	public HorairesOuverture(Integer id, String heureDeDébut, String heureDeFin) {
+	public void setPointRelais(PointRelais pointRelais) {
+		this.pointRelais = pointRelais;
+	}
+
+	public HorairesOuverture(Integer id, String heureDeDébut, String heureDeFin, Jour jour, PointRelais pointRelais) {
 		super();
 		this.id = id;
 		this.heureDeDébut = heureDeDébut;
 		this.heureDeFin = heureDeFin;
+		this.jour = jour;
+		this.pointRelais = pointRelais;
 	}
+
 	
 	
 
