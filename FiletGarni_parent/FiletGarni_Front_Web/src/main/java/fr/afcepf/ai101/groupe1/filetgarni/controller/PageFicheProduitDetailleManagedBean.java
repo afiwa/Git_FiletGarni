6aacc3509153d39a9f4925b3d89c4461108741ff
afiwa <afw2m@yahoo.fr>
2017-09-24 18:58:53 +0200

    essai d'affichage de produit détaillé à partir d'une fiche produit

---
+++ FiletGarni_parent/FiletGarni_Front_Web/src/main/java/fr/afcepf/ai101/groupe1/filetgarni/controller/PageFicheProduitDetailleManagedBean.java
@@ -0,0 +1,33 @@
+package fr.afcepf.ai101.groupe1.filetgarni.controller;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import fr.afcepf.ai101.filetGarni.business.api.IBusinessCommande;
+import fr.afcepf.ai101.groupe1.filetGarni.entity.Produit;
+
+@SessionScoped
+@ManagedBean(name = "mbPageProduitDetaille")
+public class PageFicheProduitDetailleManagedBean {
+	
+	private Produit produitSelectionne = new Produit();
+	
+	@EJB
+	IBusinessCommande businessCommande;
+
+	public String afficherFicheProduitDetaille(Integer id_produit) {
+		produitSelectionne = businessCommande.getProduitByIdWithConditionnements(id_produit);
+		System.out.println(produitSelectionne.getLibelle());
+		return "/commande/ficheProduitDetaillee/ficheProduitDetaillee.xhtml?faces-redirect=true";
+	}
+	
+	public Produit getProduitSelectionne() {
+		return produitSelectionne;
+	}
+
+	public void setProduitSelectionne(Produit paramProduitSelectionne) {
+		produitSelectionne = paramProduitSelectionne;
+	}
+	
+}
